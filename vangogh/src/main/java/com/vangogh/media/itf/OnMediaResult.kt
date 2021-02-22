6ea85e5ef6c3949fd0bb551fda5f96adb4ddc3e6
admin <xzsj@swqg>
2021-02-22 11:51:10 +0800

    优化刷新

--- vangogh/src/main/java/com/vangogh/media/itf/OnMediaResult.kt
+++ vangogh/src/main/java/com/vangogh/media/itf/OnMediaResult.kt
@@ -5,7 +5,7 @@ import com.vangogh.media.models.MediaItem
 
 /**
  * @ClassName OnMediaResult
- * @Description TODO
+ * @Description callback for Media
  * @Author aa
  * @Date 2021/1/19 10:37
  * @Version 1.0
